af8af38d364cb2987b291927fc5c0ac42750169c
Quvang <suneq18@gmail.com>
2020-02-17 14:47:21 +0100

    projekt med mongodb world dump fil

--- handlers/handler.js
+++ handlers/handler.js
@@ -1,21 +1,33 @@
 'use strict';
 /*
  * handlers.js
- * Requesthandlers to be called by the router mechanism
+ * Requesthandlers to be called by the routing mechanism
  */
-const filesystem = require("fs");                   // file system access
-module.exports = {
-    home(req, res) {
-        filesystem.readFile("./pages/index.html", function(err, data) {
-            if (err) {
-                res.end("<h1>The page you wanted doesn't exist</h1>");
+const filesystem = require("fs");                           // file system access
+const httpStatus = require("http-status-codes");
+
+const goError = function(res) {
+    res.writeHead(httpStatus.NOT_FOUND, {   // http page not found, 404
+        "Content-Type": "text/html; charset=utf-8"
+    });
+    res.write("<h1>404 Not Found</h1>");
+    res.end();
+};
+
+exports.getAndRespond = function(path, contentType, res) {
+    console.log(path);
+    if (filesystem.existsSync(path)) {              // does file exist, sync
+        filesystem.readFile(path, function(err, data) { // read
+            if (err) {                      // if read error
+                console.log("nml: " + err);           // inform server
+                goError(res);               // inform user
+                return;                     // back to caller
             }
-            res.write(data);
-            res.end();
+            res.writeHead(httpStatus.OK, contentType); // prep header
+            res.write(data);                // prep body with read data
+            res.end();                      // send response
         });
-    },
-    notfound(req, res) {
-        console.log(`Log: No handler found for route ${req.url}.`);
-        res.end();
+    } else {
+        goError(res);                       // doesnt exist error
     }
-}
+};
--- routers/router.js
+++ routers/router.js
@@ -1,26 +1,101 @@
 "use strict";
-/*
- * Tjekker om routed handler funktionen eksistere
- * If man kalder den så ja, eller så brokker den sig
- */
-const handlers = require("../handlers/handler");               // handlers module
 
-const requestHandlers = {                             // application urls here
-    "/home": handlers.home,
-    "/info": handlers.info,
-    "/contact": handlers.contact,
-    "/about": handlers.about,
-    "/hello": handlers.hello,
-    "/notfound": handlers.notfound,
-}
+const handlers = require("../handlers/handler");    // handlers module
+const httpStatus = require("http-status-codes");
+const contentTypes = {
+    "text": { "Content-Type": "text/plain; charset=utf-8" },
+    "start": { "Content-Type": "text/html; charset=utf-8" },
+    "js": { "Content-Type": "application/js" },
+    "css": { "Content-Type": "text/css" },
+    "png": { "Content-Type": "image/png" },
+    "jpg": { "Content-Type": "image/jpg" },
+    "gif": { "Content-Type": "image/gif" },
+    "ico": { "Content-Type": "image/x-icon" },
+    "svg": { "Content-Type": "image/svg+xml" }
+};
 
-module.exports = {
-    route(req, res, body) {
+const routes = {                                    // register handles to routes
+    "GET": {
+        "/start": handlers.getAndRespond,
+        "/side": handlers.getAndRespond,
+        "/about": handlers.getAndRespond,
+        "/contact": handlers.getAndRespond,
+        "js": handlers.getAndRespond,
+        "css": handlers.getAndRespond,
+        "png": handlers.getAndRespond,
+        "jpg": handlers.getAndRespond,
+        "gif": handlers.getAndRespond,
+        "ico": handlers.getAndRespond,
+        "svg": handlers.getAndRespond
+    },
 
-        if (typeof requestHandlers[req.url] === 'function') { // look for route
-            requestHandlers[req.url](req, res);               // if found use it
+    "POST": {
+        "/contact": handlers.receiveData
+    }
+};
+
+exports.route = function(req, res, body) {          // routing
+    let asset;
+    let type;
+    let routedUrl;
+    if (req.url.indexOf(".js") !== -1) {            // check for asset types
+        asset = "js";
+        routedUrl = "public/javascripts" + req.url;
+        type = contentTypes.js;
+    } else if (req.url.indexOf(".css") !== -1) {
+        asset = "css";
+        routedUrl = "public/stylesheets" + req.url;
+        type = contentTypes.css;
+    } else if (req.url.indexOf(".png") !== -1) {
+        asset = "png";
+        routedUrl = "public/images" + req.url;
+        type = contentTypes.png;
+    } else if (req.url.indexOf(".jpg") !== -1) {
+        asset = "jpg";
+        routedUrl = "public/images" + req.url;
+        type = contentTypes.jpg;
+    } else if (req.url.indexOf(".gif") !== -1) {
+        asset = "gif";
+        routedUrl = "public/images" + req.url;
+        type = contentTypes.gif;
+    } else if (req.url.indexOf(".svg") !== -1) {
+        asset = "svg";
+        routedUrl = "public/images" + req.url;
+        type = contentTypes.svg;
+    } else if (req.url.indexOf(".ico") !== -1) {
+        asset = "ico";
+        routedUrl = req.url;
+        type = contentTypes.ico;
+    } else {
+        if (req.url.charAt(req.url.length - 1) === "/") {
+            asset = "/start";
+            routedUrl = "pages/index.html";
+            type = contentTypes.html;
+        } else if (req.url === "/start") {
+            asset = req.url;
+            routedUrl = "pages/index.html";
+            type = contentTypes.html;
+        } else if (req.url === "/side") {
+            asset = req.url;
+            routedUrl = "pages/side.html";
+            type = contentTypes.html;
         } else {
-            requestHandlers["/notfound"](req, res);           // use notfound
+            asset = req.url;
+            routedUrl = "pages" + req.url + ".html";
+            type = contentTypes.html;
         }
     }
-}
+
+    try {
+        if (routes[req.method][asset]) {            // does handler exist to this route
+            console.log("routing: " + asset + " file: " + routedUrl);
+            routes[req.method][asset](routedUrl, type, res);  // yes, call it with params
+        } else {                                // no, return error msg
+            console.log("nmlnf: " + req.url);
+            res.writeHead(httpStatus.NOT_FOUND, contentTypes.text);
+            res.end(`route for <kbd>${req.url}</kbd> not found`);
+        }
+    } catch (ex) {                              // routing exception
+        console.log("Log: Routing exception: " + ex);
+    }
+};
